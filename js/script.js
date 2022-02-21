5d45714df384cacca1e2542f035ebab7d6fd76c0
edvin-bandic-nti-johanneberg <edvin.bandic@elev.ga.ntig.se>
2022-02-21 10:37:07 +0100

    början på min codning
    nu har jag börjat med min hamburgare  meny och jag har gjort mallen till den och jag känner att jag ska fortsätta att göra klart den

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navlinks')
+const burger = document.querySelector('.burger')
+
+
+function toggleMeny(click) {
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+}
+
+burger.addEventListener('click', toggleMeny)
